e1494db6c3015224338c7ef4c513a5273aa1f21b
adamahmed74 <adamahmed19600@gmail.com>
2024-03-20 17:02:59 +0200

    adam

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    variable = len(my_list)
+    if idx < 0 or idx >= variable:
+        return my_list[idx]
+    else:
+        return my_list[idx]
